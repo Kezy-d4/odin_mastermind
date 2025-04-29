173b91e2edd1217e71eac1e070c07a4b2a06ecef
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-04-29 15:56:20 -0400

    chore: fresh slate & redefine classes
    It has become apparent that it will likely later prove useful to
    distinguish between the human and the computer when it comes to which
    role they are playing in the game. This should allow the code to
    be more changeable following phase 1 when I look to add a computer code
    breaker and human code maker. I need to embrace OOP so that I can make
    those later expansions without major refactoring and rebuilding.

--- lib/code_breaker.rb
+++
@@ -1,4 +0,0 @@
-require_relative "code_peg"
-
-class CodeBreaker
-end
--- lib/game.rb
+++
@@ -1,9 +0,0 @@
-require_relative "code_maker"
-require_relative "code_breaker"
-
-class Game
-  def initialize
-    @code_maker = CodeMaker.new
-    @code_breaker = CodeBreaker.new
-  end
-end
