07274d6eb8ee1b0e1725367b16c460d42f3fa2ea
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-05-02 00:29:05 -0400

    feat: add board interface

--- lib/human_code_breaker.rb
+++ lib/human_code_breaker.rb
@@ -1,13 +1,19 @@
 class HumanCodeBreaker
   attr_reader :attempts_available
+  attr_accessor :attempts_made
 
   def initialize
     @attempts_available = 10
+    @attempts_made = 0
   end
 
   def prompt_guess
     puts "Your guess?"
     gets.chomp
+  end
+
+  def attempts_exhausted?
+    attempts_made == attempts_available
   end
 end
 
