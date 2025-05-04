79b78a5111219cf6776ba8dcdbf38162fa7de720
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-05-04 01:03:42 -0400

    chore: wipe feedback logic

--- lib/computer_code_maker.rb
+++ lib/computer_code_maker.rb
@@ -28,28 +28,6 @@ class ComputerCodeMaker
     secret_code.each { print "? ".colorize(:red) }
     puts
   end
-
-  def generate_feedback(attempt_arr)
-    feedback_arr = []
-    correct_digits(attempt_arr).times { feedback_arr << feedback_pegs[:correct] }
-    feedback_arr
-  end
-
-  def correct_digits(attempt_arr)
-    amount = 0
-    attempt_arr.each_index do |idx|
-      amount += 1 if attempt_arr[idx] == secret_code[idx]
-    end
-    amount
-  end
-
-  def digits_remaining_to_evaluate(feedback_arr)
-    secret_code_length - feedback_arr.length
-  end
-
-  def positions_of_correct_digits(attempt_arr)
-    attempt_arr.each_index.select { |idx| attempt_arr[idx] == secret_code[idx] }
-  end
 end
 
 # Testing
